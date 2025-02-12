982451bde2a90fe5c9469f62a6cacae629db1cad
HelloAmir08 <amirjontoirov03@gmail.com>
2025-02-12 19:14:36 +0500

    corrected

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from user import views
 
 urlpatterns = [
-    path("", views.login_page, name="login"),
+    path("login/", views.login_page, name="login"),
     path('logout/', views.logout_page, name='logout_page')
 ]
